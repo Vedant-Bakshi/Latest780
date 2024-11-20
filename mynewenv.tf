ca587c74f6e34d9780a58aaacae76c7ed625654f
vedantbakshi <bakshi.vedant@gmail.com>
2024-11-20 08:31:12 +0530

    VPC addition terraform code

---
+++ mynewenv.tf
@@ -0,0 +1,27 @@
+provider "aws" {
+  region = "ap-southeast-1"
+}
+resource "aws_vpc" "myvpc" {
+  cidr_block       = "10.10.0.0/16"
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "Myvpc"
+  }
+}
+resource "aws_subnet" "main" {
+  vpc_id     = aws_vpc.myvpc.id
+  cidr_block = "10.10.1.0/24"
+
+  tags = {
+    Name = "Public"
+  }
+}
+resource "aws_subnet" "private" {
+  vpc_id     = aws_vpc.myvpc.id
+  cidr_block = "10.10.2.0/24"
+
+  tags = {
+    Name = "Private"
+  }  
+}
